506de5b8ced844218e29b535277eedb29afaf4e2
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2024-08-31 16:18:13 +0530

    login and register page with validation

---
+++ picker/app/login/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginPage from '@/components/LoginPage'
+
+function page() {
+  return (
+    <>
+    <LoginPage mode='login'/>
+    </>
+  )
+}
+
+export default page
---
+++ picker/app/register/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginPage from '@/components/LoginPage'
+
+function page() {
+  return (
+    <LoginPage mode='register' />
+  )
+}
+
+export default page
